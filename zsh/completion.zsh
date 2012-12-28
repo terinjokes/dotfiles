4bfb17a5cff7ec19c80da53211cc8a60e0502dac
Terin Stock <terinjokes@gmail.com>
2012-12-28 11:36:14 -0500

    In the words of the great @holman: "fuck yeah completions"

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,5 +1,8 @@
 # matches case insensitive for lowercase
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
-# pasting with tabs desn't perform completion
+# pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# enable menu selection
+zstyle ':completion:*' menu select
