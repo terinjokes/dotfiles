67ce5a4fe31c063c99dc62eee7bbb42c422887e9
Terin Stock <terinjokes@gmail.com>
2013-01-03 12:27:34 -0800

    Edit the PATH as an array/set.

--- system/pathes.zsh
+++ system/pathes.zsh
@@ -1,3 +1,6 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+path=($HOME/.rbenv/shims $HOME/.rbenv/bin /usr/local/bin /usr/local/sbin $ZSH/bin $path)
+path=($^path(N)) # Remove non-existing dirs
+path=(./bin $path) # Add ./bin to the path
+typeset -U path # Keep only the first occurance
 
 export MANPATH="/usr/local/share/man:$MANPATH"
