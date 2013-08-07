01711d6103b59770cc0fe5b91aaea78639ccbad1
Terin Stock <terinjokes@gmail.com>
2013-08-06 17:10:01 -0700

    Migrating to chruby to rbenv

---
+++ ruby/chruby.zsh
@@ -0,0 +1,12 @@
+if [[ -d /usr/local/share/chruby/ ]]; then
+	# Linux installation of chruby
+	chruby_path=/usr/local/share/chruby/
+elif [[ -d /usr/local/opt/chruby/share/chruby/ ]]; then
+	# Homebrew installation of chruby
+	chruby_path=/usr/local/opt/chruby/share/chruby/
+fi
+
+if [[ -d $chruby_path ]]; then
+	source $chruby_path/chruby.sh
+	source $chruby_path/auto.sh
+fi
--- system/pathes.zsh
+++ system/pathes.zsh
@@ -1,4 +1,4 @@
-path=($HOME/.rbenv/shims $HOME/.rbenv/bin /usr/local/bin /usr/local/sbin $ZSH/bin $path)
+path=(/usr/local/bin /usr/local/sbin $ZSH/bin $path)
 path=($^path(N)) # Remove non-existing dirs
 path=(./bin $path) # Add ./bin to the path
 typeset -U path # Keep only the first occurance
