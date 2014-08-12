6c3834f9cfba537f1d56abc9fa34abe1c8cfc472
Terin Stock <terinjokes@gmail.com>
2014-08-12 00:48:47 -0700

    Support for, and preference given to, `gh` over `hub`.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,6 +1,8 @@
 # Use `hub` git wrapper, if installed
+gh_path=$(which gh)
 hub_path=$(which hub)
-if [[ -f $hub_path ]]
-then
-  alias git=$hub_path
+if [[ -f $gh_path ]]; then
+  alias git=$gh_path
+elif [[ -f $hub_path ]]; then
+	alias git=$hub_path
 fi
